013223cd8355e706a477fc1dc41d866e5a1df34b
jesusyancarlo <yancarlogoga@gmail.com>
2026-08-26 12:52:16 -0700

    Corrección 2

--- js/functions.js
+++ js/functions.js
@@ -18,22 +18,22 @@ var contador = 0;
     }
 
 function cambiar(r, c) {
-    //alert(r+""+c)
-    var celda = document.getElementById("tablero");
-    var r1=1, c1=c, r2=r, c2=c;
-    var r3=r, c3=c, r4=r, c4=c;
+    var tablero = document.getElementById("tablero");
+
+    var r1 = r, c1 = c;
+    var r2 = r, c2 = c;
+    var r3 = r, c3 = c;
+    var r4 = r, c4 = c;
+
     for (let i = 0; i < 8; i++) {
-        celda.rows[r].cells[i].style.backgroundColor="red";
-        celda.rows[i].cells[c].style.backgroundColor="red";
 
-    if (r1<8 & c1<8) celda.rows[r1++].cells[c1++].style.backgroundColor="red";
+        tablero.rows[r].cells[i].style.backgroundColor = "red";
+        tablero.rows[i].cells[c].style.backgroundColor = "red";
 
-    if (r2<8 & c2>=0) celda.rows[r2++].cells[c2--].style.backgroundColor="red"; //
-
-    if (r3>=0 & c3>=0) celda.rows[r3--].cells[c3--].style.backgroundColor="red";
-
-    if (r4>=0 & c4<8) celda.rows[r4--].cells[c4++].style.backgroundColor="red"; //
-
+        if (r1 < 8 && c1 < 8) tablero.rows[r1++].cells[c1++].style.backgroundColor = "red";
+        if (r2 < 8 && c2 >= 0) tablero.rows[r2++].cells[c2--].style.backgroundColor = "red";
+        if (r3 >= 0 && c3 >= 0) tablero.rows[r3--].cells[c3--].style.backgroundColor = "red";
+        if (r4 >= 0 && c4 < 8) tablero.rows[r4--].cells[c4++].style.backgroundColor = "red";
     }
 }
 
